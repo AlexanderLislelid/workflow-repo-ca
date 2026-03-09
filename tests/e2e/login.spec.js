7e96d43082a2ad383d5a31391f9e5f4865589374
AlexanderLislelid <alexlislelid@gmail.com>
2026-03-09 14:07:12 +0100

    test login with valid credentials from .env variableS

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,7 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test("login page has correct title", async ({ page }) => {
+test("user can log in with valid credentials", async ({ page }) => {
   await page.goto("http://127.0.0.1:5500/login/");
 
-  await expect(page).toHaveTitle(/login/i);
+  await page
+    .getByRole("textbox", { name: "Email" })
+    .fill(process.env.TEST_USER_EMAIL);
+
+  await page
+    .getByRole("textbox", { name: "Password" })
+    .fill(process.env.TEST_USER_PASSWORD);
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
 });
