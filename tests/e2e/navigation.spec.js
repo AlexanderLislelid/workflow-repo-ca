728ae7a70453687319b1719817d2a1b93c9dea7d
AlexanderLislelid <alexlislelid@gmail.com>
2026-03-09 14:48:07 +0100

    add test for opening venue details page

---
+++ tests/e2e/navigation.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("User can open venue details page", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/login");
+
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link").nth(4).click();
+
+  await expect(
+    page.getByRole("heading", { name: "Venue details:" }),
+  ).toBeVisible();
+});
