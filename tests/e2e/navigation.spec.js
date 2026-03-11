52fcbabfa4d767a6047c1eace5ff0d05958f7fb7
AlexanderLislelid <alexlislelid@gmail.com>
2026-03-11 11:44:13 +0100

    update selector for venue details link in navigation test to make it more robust

--- tests/e2e/navigation.spec.js
+++ tests/e2e/navigation.spec.js
@@ -4,7 +4,7 @@ test("User can open venue details page", async ({ page }) => {
   await page.goto("http://127.0.0.1:5500/login");
 
   await page.getByRole("link", { name: "Home" }).click();
-  await page.getByRole("link").nth(4).click();
+  await page.locator("#venue-container a").first().click();
 
   await expect(
     page.getByRole("heading", { name: "Venue details:" }),
